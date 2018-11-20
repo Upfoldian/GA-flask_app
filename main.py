3702dde429d04d4c001a3dc97f4984ec749eae0b
Thomas Upfold <thomasupfold@gmail.com>
2018-11-20 21:32:32 +1100

    working on test datafile, now to move onto uploaded data

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ app = Flask(__name__)
 
 @app.route('/', methods = ['GET'])
 def display_data():
-	solarDesc = {"time": ("date", "Time"), "trackV": ("number", "Tracked Voltage"), "untrackV": ("number", "Untracked Voltage")}
+	solarDesc = {"time": ("datetime", "Time"), "trackV": ("number", "Tracked Voltage"), "untrackV": ("number", "Untracked Voltage")}
 	solarData = [];
 	with open('thursday test.csv') as csvfile:
 		#fieldnames = ['created_at','entry_id','people','temp','humid','trackV','trackI','untrackV','untrackI','motorpos' ]
@@ -17,15 +17,22 @@ def display_data():
 		for row in reader:
 			time = datetime.strptime(row['created_at'],  "%Y-%m-%d %H:%M:%S %Z").replace(tzinfo=timezone('UTC'))
 			time = time.astimezone(timezone('Australia/Sydney'))
-			print time
-			if row['trackV'] == "":
-				row['trackV'] = 0 
-			if row['untrackV'] == "":
-				row['untrackV'] = 0
+			trackV = row['trackV']
+			untrackV = row['untrackV']
+
+			if trackV != "":
+				trackV = int(trackV)
+			else:
+				trackV = None
+
+			if untrackV != "":
+				untrackV = int(untrackV)
+			else:
+				untrackV = None
 
 
-			solarData.append({"time": time, "trackV": int(row['trackV']), "untrackV": int(row['untrackV'])})
-	print (solarData[-1])
+
+			solarData.append({"time": time, "trackV": trackV, "untrackV": untrackV})
 	data_table = gviz_api.DataTable(solarDesc)
 	data_table.LoadData(solarData)
   	json = data_table.ToJSon(columns_order=("time", "trackV", "untrackV"), order_by="time")
