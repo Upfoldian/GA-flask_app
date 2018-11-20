1b19247df0503699dd3a53f4ccf17a05928a046a
Thomas Upfold <thomasupfold@gmail.com>
2018-11-20 12:24:41 +1100

    getting there, need to tweak time data still to get working plot

--- main.py
+++ main.py
@@ -1,21 +1,34 @@
 from flask import Flask, render_template
 import gviz_api
+import csv
 
-
+from datetime import datetime
+from pytz import timezone
 
 app = Flask(__name__)
 
 @app.route('/', methods = ['GET'])
 def display_data():
-	description = {"year": ("string", "Year"), "sales": ("number", "Sales"), "expenses": ("number", "Expenses")}
-	data = [{"year": '2014', "sales": 1000, "expenses": 400},
-    	    {"year": '2015', "sales": 2000, "expenses": 500},
-    	    {"year": '2016', "sales": 3000, "expenses": 300},
-    	    {"year": '2017', "sales": 1000, "expenses": 200}]
+	solarDesc = {"time": ("date", "Time"), "trackV": ("number", "Tracked Voltage"), "untrackV": ("number", "Untracked Voltage")}
+	solarData = [];
+	with open('thursday test.csv') as csvfile:
+		#fieldnames = ['created_at','entry_id','people','temp','humid','trackV','trackI','untrackV','untrackI','motorpos' ]
+		reader = csv.DictReader(csvfile)
+		for row in reader:
+			time = datetime.strptime(row['created_at'],  "%Y-%m-%d %H:%M:%S %Z").replace(tzinfo=timezone('UTC'))
+			time = time.astimezone(timezone('Australia/Sydney'))
+			print time
+			if row['trackV'] == "":
+				row['trackV'] = 0 
+			if row['untrackV'] == "":
+				row['untrackV'] = 0
 
-	data_table = gviz_api.DataTable(description)
-	data_table.LoadData(data)
-  	json = data_table.ToJSon(columns_order=("year", "sales", "expenses"), order_by="year")
+
+			solarData.append({"time": time, "trackV": int(row['trackV']), "untrackV": int(row['untrackV'])})
+	print (solarData[-1])
+	data_table = gviz_api.DataTable(solarDesc)
+	data_table.LoadData(solarData)
+  	json = data_table.ToJSon(columns_order=("time", "trackV", "untrackV"), order_by="time")
   	return render_template('display.j2', chartJSON=json)
 
 @app.route('/', methods = ['POST'])
