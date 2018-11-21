0beada483f4408cd79103c40b6b89ee151262590
Thomas Upfold <thomasupfold@gmail.com>
2018-11-21 14:40:01 +1100

    asdf x2

--- GAapp.py
+++ GAapp.py
@@ -28,4 +28,4 @@ def display_data():
   		return 'how did you get here?'
 
 if __name__ == '__main__':
-    app.run(port=5000) #run app in debug mode on port 5000 
+    app.run(host='0.0.0.0',port=5000) #run app in debug mode on port 5000 
