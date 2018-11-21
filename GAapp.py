6f38759c2e93eb56ff7cd9efcb02493e546d26b6
Thomas Upfold <thomasupfold@gmail.com>
2018-11-21 21:47:35 +1100

    added some very basic error catching

--- GAapp.py
+++ GAapp.py
@@ -14,18 +14,24 @@ def display_data():
 
 
 	if request.method == 'GET':
-		uploadDesc = {"time": ("datetime", "Time"), "value": ("number", "value")}
-		uploadData = getUploadDict('upload.csv')
-		data_table = gviz_api.DataTable(uploadDesc)
-		data_table.LoadData(uploadData)
-		uploadJSON = data_table.ToJSon(columns_order=("time", "value"), order_by="time")
-  		return render_template('display.j2', static=staticJSON, upload=uploadJSON)
+		try:
+			uploadDesc = {"time": ("datetime", "Time"), "value": ("number", "value")}
+			uploadData = getUploadDict('upload.csv')
+			data_table = gviz_api.DataTable(uploadDesc)
+			data_table.LoadData(uploadData)
+			uploadJSON = data_table.ToJSon(columns_order=("time", "value"), order_by="time")
+	  		return render_template('display.j2', static=staticJSON, upload=uploadJSON)
+	  	except:
+	  		return "uh oh"
   	elif request.method == 'POST':
-  		value = int(request.form['value'])
-  		updateUploadCSV('upload.csv', datetime.now(), value)
-		return 'uploaded value %d' % value
+  		try:
+  			value = int(request.form['value'])
+  			updateUploadCSV('upload.csv', datetime.now(), value)
+			return 'uploaded value %d' % value
+		except:
+			return "error, make sure your post is something like value=5"
   	else:
-  		return 'how did you get here?'
+  		return 'not a GET/POST friend'
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0',port=5000) #run app in debug mode on port 5000 
